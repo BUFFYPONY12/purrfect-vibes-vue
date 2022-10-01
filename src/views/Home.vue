8b0156d95bf544881cc2118cc2c8b076ac94ae2f
Pakin216110003 <pakin@star-board.com>
2022-10-02 00:27:10 +0700

    Setup project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,26 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
+  <p @click="changelang()">{{ $t("message") }}</p>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import i18n from "../i18n";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
+  },
+  setup() {
+    const changelang = () => {
+      console.log(i18n.global.locale.value);
+    };
+    return {
+      changelang,
+    };
   },
 };
 </script>
